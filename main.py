f95a993a937a90a0c1ce3158ec4a45960c27c18c
imabari <32343845+imabari@users.noreply.github.com>
2021-06-08 14:09:36 +0900

    全部に団体コードを追加

--- main.py
+++ main.py
@@ -31,8 +31,8 @@ musen = [
     {"auth": "02_tohoku", "value": "I"},
     {"auth": "03_kanto", "value": "A"},
     {"auth": "04_shinetsu", "value": "B"},
-    {"auth": "05_hokuriku", "value": "D"},
-    {"auth": "06_tokai", "value": "C"},
+    {"auth": "05_hokuriku", "value": "C"},
+    {"auth": "06_tokai", "value": "D"},
     {"auth": "07_kinki", "value": "E"},
     {"auth": "08_chugoku", "value": "F"},
     {"auth": "09_shikoku", "value": "G"},
@@ -40,14 +40,16 @@ musen = [
     {"auth": "11_okinawa", "value": "O"},
 ]
 
-# 団体コード
 df_code = pd.read_csv(
-    "https://docs.google.com/spreadsheets/d/e/2PACX-1vSseDxB5f3nS-YQ1NOkuFKZ7rTNfPLHqTKaSag-qaK25EWLcSL0klbFBZm1b6JDKGtHTk6iMUxsXpxt/pub?gid=0&single=true&output=csv",
+    "https://docs.google.com/spreadsheets/d/e/2PACX-1vSseDxB5f3nS-YQ1NOkuFKZ7rTNfPLHqTKaSag-qaK25EWLcSL0klbFBZm1b6JDKGtHTk6iMUxsXpxt/pub?gid=284869672&single=true&output=csv",
     dtype={"団体コード": int, "都道府県名": str, "郡名": str, "市区町村名": str},
 )
 
-df_code["市区町村名"] = df_code["郡名"].fillna("") + df_code["市区町村名"]
+df_code["市区町村名"] = df_code["郡名"].fillna("") + df_code["市区町村名"].fillna("")
 df_code.drop("郡名", axis=1, inplace=True)
+
+df_code
+
 
 def fetch_api(parm, auth):
 
@@ -86,10 +88,22 @@ def fetch_api(parm, auth):
     # 都道府県に移動
     df2["都道府県名"] = df2["市区町村名"].where(flag).fillna(method="ffill")
 
+    df2["市区町村名"] = df2["市区町村名"].mask(flag, "")
+
     df2["更新日"] = update.isoformat()
 
-    # 列名並び替え
-    df3 = df2.reindex(["都道府県名", "市区町村名", "開設局数", "更新日"], axis=1)
+    # 団体コードを付加
+    df3 = (
+        pd.merge(df2, df_code, on=["都道府県名", "市区町村名"], how="left")
+        .reset_index(drop=True)
+        .reindex(["団体コード", "都道府県名", "市区町村名", "開設局数", "更新日"], axis=1)
+    )
+
+    df3["団体コード"] = df3["団体コード"].astype("Int64")
+
+    df3.sort_values("団体コード", inplace=True)
+
+    df3.to_csv(f"{auth}.csv", index=False, encoding="utf_8_sig")
 
     # 都道府県
 
@@ -103,14 +117,7 @@ def fetch_api(parm, auth):
 
     # 市区町村
 
-    df_cities = (
-        pd.merge(df3[~flag], df_code, on=["都道府県名", "市区町村名"], how="left")
-        .reset_index(drop=True)
-        .reindex(["団体コード", "都道府県名", "市区町村名", "開設局数", "更新日"], axis=1)
-    )
-    df_cities["団体コード"] = df_cities["団体コード"].astype("Int64")
-
-    df_cities.sort_values("団体コード", inplace=True)
+    df_cities = df3[~flag].reset_index(drop=True)
 
     df_cities.to_csv(
         pathlib.Path("data", f"{auth}_cities.csv"), index=False, encoding="utf_8_sig"
